bd06cc69f8b5b02455e74ad0185cdc51067cbebf
Eze-odurukwe Adaeze <daizyodurukwe@gmail.com>
2019-09-27 19:45:00 +0100

    Update balancedParentheses.js

--- balancedParentheses.js
+++ balancedParentheses.js
@@ -1,47 +1,41 @@
 function processData(input) {
-  // Enter your code here
-  // Sample input '[{()}]'
-  
-  // Define variables
-  const openParenthesis = '(';
-  const closeParentesis = ')';
-  const parentesisSequence = '()';
-  const openBracket = '{';
-  const closeBracket = '}';
-  const bracketSequence = '{}';
-  const openSquare ='[';
-  const closeSquare = ']';
-  const squareSequence = '[]';
-  
-  const lengthOfArray = input.length;
-  const arrayOfString = input.split('');
-  
-  const output = '';
-  
-  const miniArray = [];
-      
-  const error = 0;
-  const matching = 1;
-  
-  // Throw error if the number of characters are odd
-  if(lengthOfArray % 2 !== 0) return error;
-  
-  // Throw error if the beginning of the string in a closing brace, bracket or parenthesis
-  if(arrayOfString[0] === closeBracket || arrayOfString === closeSquare || arrayOfString === closeBracket) return error;
-  
-  // Sort the array of individual characters to have corresponding closing brackets next to them
-  // Create mini arrays with corresponding indexes and compare with correct string sequence
-  
-  for (i = 0; i < arrayOfString.length; i++) {
-      for (j = arrayOfString.length; j > 0 ; j--) {
-          miniArray.push(arrayOfString[i], arrayOfString[j])
-          if(miniArray.toString() !== parentesisSequence || miniArray.toString() !== bracketSequence || miniArray.toString() !== squareSequence) {
-              output = error;
-          } else {
-            output = matching;
-            continue;
-          }
+    // Sample input '[{()}]'
+    
+    const openParentheses = '(';
+    const closeParenteses = ')';
+    const parentesesSequence = '(,)';
+    const openBracket = '{';
+    const closeBracket = '}';
+    const bracketSequence = '{,}';
+    const openSquare ='[';
+    const closeSquare = ']';
+    const squareSequence = '[,]';
+    const lengthOfArray = input.length;
+    const arrayOfString = input.split('');
+    const error = 0;
+    const matching = 1;
+    let output = '';
+    let miniArray = [];
+        
+    
+    
+    if(lengthOfArray % 2 !== 0) return error;
+    
+    if(arrayOfString[0] === closeBracket || arrayOfString === closeSquare || arrayOfString === closeBracket) return error;
+    
+    for (let i = 0; i < arrayOfString.length / 2; i++) {
+      miniArray = []
+      let j = (arrayOfString.length - 1) - i;
+      miniArray.push(arrayOfString[i], arrayOfString[j]);
+      console.log(miniArray.toString());
+      if((miniArray.toString() === parentesesSequence) || (miniArray.toString() === bracketSequence) || (miniArray.toString() === squareSequence)) {
+        output = matching;
+      } else {
+        output = error;
+        break;
       }
-  }
-  return output;
-} 
+    }
+    return output;
+};
+
+console.log(processData('[{()}]'));
